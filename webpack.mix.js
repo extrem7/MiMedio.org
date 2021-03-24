3f2e3fef1a8627ac6bf383b377b6a131b934e859
Yurii Kinakh <extrem7@meta.ua>
2021-03-24 08:55:49 +0200

    Upgrade laravel-mix to 6 version

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,14 @@
 const mix = require('laravel-mix')
 const config = require('./webpack.config')
 mix.webpackConfig(config)
-mix.options({processCssUrls: false})
+
+const options = {
+    processCssUrls: false,
+    terser: {
+        extractComments: false,
+    }
+}
+mix.options(options)
 
 // admin mix
 mix.sass('resources/admin/main.scss', 'public/admin/css/main.css')
@@ -13,19 +20,17 @@ mix.scripts([
 ], 'public/admin/js/main.js')
 
 mix.copy('node_modules/pace-js/themes/blue/pace-theme-minimal.css', 'public/admin/css/pace.css')
-mix.copy('node_modules/pace-js/pace.min.js', 'public/admin/js/pace.js')
-
-mix.copy('node_modules/admin-lte/dist/img', 'public/admin/img')
-mix.copy('node_modules/admin-lte/plugins', 'public/admin/plugins')
-
-mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/admin/webfonts')
+    .copy('node_modules/pace-js/pace.min.js', 'public/admin/js/pace.js')
+    .copyDirectory('node_modules/admin-lte/dist/img', 'public/admin/img')
+    //.copyDirectory('node_modules/admin-lte/plugins', 'public/admin/plugins')
+    .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/admin/webfonts')
 
 //frontend mix
 
-mix.sass('resources/sass/app.scss', 'public/assets/css/').version()
-mix.sass('resources/frontend/scss/main.scss', 'public/assets/css/').version()
+mix.sass('resources/sass/app.scss', 'public/assets/css/')
+    .sass('resources/frontend/scss/main.scss', 'public/assets/css/')
 
-mix.js('resources/js/app.js', 'public/assets/js/').version().sourceMaps()
+mix.js('resources/js/app.js', 'public/assets/js/').vue()
 
 mix.scripts([
     'node_modules/jquery/dist/jquery.min.js',
@@ -35,10 +40,12 @@ mix.scripts([
     'node_modules/jquery-mousewheel/jquery.mousewheel.js',
     'resources/frontend/js/main.js',
     'resources/frontend/js/custom.js',
-], 'public/assets/js/main.js').version()
+], 'public/assets/js/main.js')
 
-mix.copy('resources/frontend/img', 'public/assets/img')
+mix.copyDirectory('resources/frontend/img', 'public/assets/img')
+    .copyDirectory('node_modules/tinymce/', 'public/assets/vendor/tinymce')
+    .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/assets/webfonts')
 
-mix.copy('node_modules/tinymce/', 'public/assets/vendor/tinymce')
-
-mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/assets/webfonts')
+mix.sourceMaps()
+    .version()
+    .disableSuccessNotifications()
